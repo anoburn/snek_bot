12aadad80dad3058443d046ecbd11f9da5e48c3d
Stephen Schaumann <stephen.schaumann@arcor.de>
2018-03-08 16:44:38 +0100

    Changed 'runs' from list to dictionary, added command del_run

--- snake_bot.py
+++ snake_bot.py
@@ -6,9 +6,10 @@ import snake
 import pickle
 from collections import namedtuple
 import os
+import shutil
 
 
-logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=logging.ERROR)
+logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 updater = Updater(token="559384287:AAGt9y-Cyl7CmW_8nr4a3W0yL6bpbu9NHxM")
 dispatcher = updater.dispatcher
@@ -27,14 +28,14 @@ def start_run(bot, update):
     max_gen   =  int(input[3])
 
     global n_runs
-    thread_name = "run_%i"%(n_runs)
+    run_name = "run_%i"%(n_runs)
     n_runs += 1
 
 
-    thread = Process(name=thread_name, target=run_thread, args=(bot, update, thread_name, mut_rate, mut_dev, max_gen))
+    thread = Process(name=run_name, target=run_thread, args=(bot, update, run_name, mut_rate, mut_dev, max_gen))
 
-    run = Run(thread_name, mut_rate, mut_dev, max_gen)
-    runs.append(run)
+    run = Run(run_name, mut_rate, mut_dev, max_gen)
+    runs[run_name] = run
     save_runs()
 
     threads[thread_name] = thread
@@ -64,10 +65,10 @@ def get_active(bot, update):
     chat_id = update.message.chat_id
     text = "Current runs:\n"
 
-    for run in runs:
-        if(run.name in threads):
+    for run_name in runs:
+        if(run_name in threads):
             if(threads[run.name].is_alive()):
-                text += "%s:   mut_rate = %.2f   mut_dev = %.2f   max_gen = %i\n"%(run.name, run.mut_rate, run.mut_dev, run.max_gen)
+                text += "%s:   mut_rate = %.2f   mut_dev = %.2f   max_gen = %i\n"%(run_name, run.mut_rate, run.mut_dev, run.max_gen)
 
     bot.send_message(chat_id=chat_id, text=text)
 
@@ -80,7 +81,8 @@ def get_runs(bot, update):
     chat_id = update.message.chat_id
     text = "All runs:\n"
 
-    for run in runs:
+    for run_name in runs:
+        run = runs[run_name]
         text += "%s:   mut_rate = %.2f   mut_dev = %.2f   max_gen = %i\n"%(run.name, run.mut_rate, run.mut_dev, run.max_gen)
 
     bot.send_message(chat_id=chat_id, text=text)
@@ -94,13 +96,15 @@ def get_picture(bot, update):
     chat_id = update.message.chat_id
 
     input = update.message.text.split()
-    thread_name = input[1]
-    path = "./data/%s/fitness.png"%(thread_name)
 
-    if(os.path.isfile(path)):
-        bot.sendPhoto(chat_id=chat_id, photo=open(path, "rb"))
-    else:
-        bot.send_message(chat_id=chat_id, text="No file found for run name")
+    for run_name in input[1:]:
+
+        path = "./data/%s/fitness.png"%(run_name)
+
+        if(os.path.isfile(path)):
+            bot.sendPhoto(chat_id=chat_id, caption=run_name, photo=open(path, "rb"))
+        else:
+            bot.send_message(chat_id=chat_id, text="No file found for %s"%run_name)
         
 
 dispatcher.add_handler(CommandHandler("get_picture", get_picture))
@@ -121,6 +125,25 @@ def stop_run(bot, update):
 dispatcher.add_handler(CommandHandler("stop_run", stop_run))
 
 
+def del_run(bot, update):
+    chat_id = update.message.chat_id
+    
+    input = update.message.text.split()
+
+    for run_name in input[1:]:
+        if(run_name not in runs):
+            bot.send_message(chat_id=chat_id, text="%s not found"%run_name)
+        else:
+            if(run_name in threads):
+                if(threads[run_name].is_alive()):
+                    threads[run_name].terminate()
+                threads.pop(run_name)
+            runs.pop(run_name)
+            shutil.rmtree("./data/%s"%run_name)
+            bot.send_message(chat_id=chat_id, text="%s and all corresponding data have been deleted"%run_name)
+
+dispatcher.add_handler(CommandHandler("del_run", del_run))
+
 
 if __name__ == "__main__": 
 
@@ -131,7 +154,7 @@ if __name__ == "__main__":
         file = open("./data/runs.obj", "rb")
         runs = pickle.load(file)
     else:
-        runs = []
+        runs = {}
 
     threads = {}
 
